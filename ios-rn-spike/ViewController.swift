ddf446bba2843ecc714396fc21c8b5566db861cc
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2021-03-31 14:50:48 +1100

    initial attempt

--- ios-rn-spike/ViewController.swift
+++ ios-rn-spike/ViewController.swift
@@ -6,9 +6,31 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
+    @IBAction func highScoreButtonTapped(sender : UIButton) {
+      NSLog("Hello")
+      let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+      let mockData:NSDictionary = ["scores":
+          [
+              ["name":"Alex", "value":"42"],
+              ["name":"Joel", "value":"10"]
+          ]
+      ]
+
+      let rootView = RCTRootView(
+          bundleURL: jsCodeLocation,
+          moduleName: "RNHighScores",
+          initialProperties: mockData as [NSObject : AnyObject],
+          launchOptions: nil
+      )
+      let vc = UIViewController()
+      vc.view = rootView
+      self.present(vc, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ ios-rn-spike/test.m
@@ -0,0 +1,8 @@
+//
+//  test.m
+//  ios-rn-spike
+//
+//  Created by pubudu on 31/3/21.
+//
+
+#import <Foundation/Foundation.h>
